93d77a9ea8cf9bc2e5d2302aadbd81d802660848
NhatTran-97 <nhat.tranduy1997@gmail.com>
2025-11-06 22:52:01 +0700

    Add Balancing Robot Control

---
+++ Firmware_Balancing_Robot/MDK-ARM/RTE/_Self_Balancing_Robot/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Self_Balancing_Robot' 
+ * Target:  'Self_Balancing_Robot' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
